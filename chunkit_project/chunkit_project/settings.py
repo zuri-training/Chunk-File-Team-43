ccb9892605846417ea097c7924d7a27b1328c740
Viralvic <104889740+Viralvic@users.noreply.github.com>
2022-07-28 18:50:03 +0100

    Templates, static files created and mapped

--- chunkit_project/chunkit_project/settings.py
+++ chunkit_project/chunkit_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'chunkit_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'chunkit_project/static')]
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
