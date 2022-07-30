093b6af3a3d59a6bc8a862fdd07063f19b5dc638
Viralvic <104889740+Viralvic@users.noreply.github.com>
2022-07-30 03:56:48 +0100

    Fix: Database Models Updated

--- chunkit_project/accounts/admin.py
+++ chunkit_project/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import chunkit_users
+from .models import authentication
+from .models import Chunkedfile
 
 # Register your models here.
+admin.site.register(chunkit_users)
+admin.site.register(authentication)
+admin.site.register(Chunkedfile)
